976616d32dbe697a54a951c386cd3e8cb327f165
DamianoD88 <dispe88@live.it>
2021-05-27 16:22:38 +0200

    add iniziare calcolo sconti

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-var 
+var km = prompt('Quanti Km?') 
+
+var eta = prompt('Quanti anni hai?') 
+
+if (eta < 18) {
+    
+    console.log( 'minorenne' )
+
+}else if ( eta >= 65) {
+
+    console.log( 'over65')
+
+}else{
+
+    var prezzo = 0.21 * parseInt( km );
+}
+
+
+console.log( prezzo )
+
